03f2846d67296fd7c83f9e15a4ffd8e97dda1748
henriettea <henriettearnesen@gmail.com>
2026-09-23 11:38:51 +0200

    Add files via upload

---
+++ Fra_C_til_F_ver01.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 19:21:18 2026
+
+@author: henri
+"""
+
+#%% Omregning fra C til F
+
+C = 100.0 # [grader C]
+F = C*1.8 + 32 # [grader F]
+
+#%% Utskrift
+
+print('C = ', C, 'og F =', F)
+
